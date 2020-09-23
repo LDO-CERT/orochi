71847c49a88c6e3e5622a013b89ec030290efd01
dadokkio <dadokkio@gmail.com>
2020-09-23 15:53:47 +0200

    debug libs not available in workers

--- config/settings/local.py
+++ config/settings/local.py
@@ -37,19 +37,30 @@ INSTALLED_APPS = ["whitenoise.runserver_nostatic"] + INSTALLED_APPS  # noqa F405
 
 # django-debug-toolbar
 # ------------------------------------------------------------------------------
-INSTALLED_APPS += ["debug_toolbar"]  # noqa F405
-MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa F405
-DEBUG_TOOLBAR_CONFIG = {
-    "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
-    "SHOW_TEMPLATE_CONTEXT": True,
-}
-INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
-if env("USE_DOCKER") == "yes":
-    import socket
+try:
+    import debug_toolbar
 
-    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
-    INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
+    INSTALLED_APPS += ["debug_toolbar"]  # noqa F405
+
+    MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa F405
+    DEBUG_TOOLBAR_CONFIG = {
+        "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
+        "SHOW_TEMPLATE_CONTEXT": True,
+    }
+    INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
+    if env("USE_DOCKER") == "yes":
+        import socket
+
+        hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+        INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
+except ImportError:
+    pass
 
 # django-extensions
 # ------------------------------------------------------------------------------
-INSTALLED_APPS += ["django_extensions"]  # noqa F405
+try:
+    import django_extensions
+
+    INSTALLED_APPS += ["django_extensions"]  # noqa F405
+except ImportError:
+    pass
