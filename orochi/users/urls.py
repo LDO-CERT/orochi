7a1d100404df06c20547209cfb2ecc17de478e8f
dadokkio <dadokkio@gmail.com>
2020-09-30 16:48:14 +0200

    restore users views

--- orochi/users/urls.py
+++ orochi/users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from orochi.users.views import UserDetailView, UserRedirectView
+from orochi.users.views import (
+    user_detail_view,
+    user_redirect_view,
+)
 
 app_name = "users"
 urlpatterns = [
-    path("~redirect/", view=UserDetailView.as_view(), name="redirect"),
-    path("<str:username>/", view=UserRedirectView.as_view(), name="detail"),
+    path("~redirect/", view=user_redirect_view, name="redirect"),
+    path("<str:username>/", view=user_detail_view, name="detail"),
 ]
--- orochi/users/views.py
+++ orochi/users/views.py
@@ -1,13 +1,20 @@
+from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
 from django.views.generic import RedirectView, DetailView
+
+User = get_user_model()
 
 
 class UserDetailView(LoginRequiredMixin, DetailView):
-    queryset = get_user_model().objects.prefetch_related("plugins__plugin").all()
+    queryset = User.objects.prefetch_related("plugins__plugin").all()
     slug_field = "username"
     slug_url_kwarg = "username"
+
+
+user_detail_view = UserDetailView.as_view()
 
 
 class UserRedirectView(LoginRequiredMixin, RedirectView):
@@ -15,3 +22,6 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
 
     def get_redirect_url(self):
         return reverse("users:detail", kwargs={"username": self.request.user.username})
+
+
+user_redirect_view = UserRedirectView.as_view()
