b245a4e516f11735083e2a75287ab6de377e43c3
dadokkio <dadokkio@gmail.com>
2020-09-30 10:17:15 +0200

    restrict user api only to admins

--- orochi/users/api/views.py
+++ orochi/users/api/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.decorators import action
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
 from rest_framework.response import Response
@@ -14,9 +14,7 @@ class UserViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, GenericV
     serializer_class = UserSerializer
     queryset = User.objects.all()
     lookup_field = "username"
-
-    def get_queryset(self, *args, **kwargs):
-        return self.queryset.filter(id=self.request.user.id)
+    permission_classes = [permissions.IsAdminUser]
 
     @action(detail=False, methods=["GET"])
     def me(self, request):
