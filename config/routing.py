099d23e6652c9cbec3eb85903dcb070e0586cf60
dadokkio <dadokkio@gmail.com>
2020-11-02 11:35:36 +0100

    channel 3.0 compatibility

--- config/routing.py
+++ config/routing.py
@@ -1,9 +1,11 @@
 import website.routing
+from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 application = ProtocolTypeRouter(
     {
+        "http": get_asgi_application(),
         "websocket": AuthMiddlewareStack(
             URLRouter(website.routing.websocket_urlpatterns)
         ),
--- orochi/website/routing.py
+++ orochi/website/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/notify/(?P<user_id>\w+)/$", consumers.NotifyConsumer),
+    re_path(r"ws/notify/(?P<user_id>\w+)/$", consumers.NotifyConsumer.as_asgi()),
 ]
