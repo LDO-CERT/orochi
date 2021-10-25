afc691e572b489d144f56db8772766bdd91a26fb
dadokkio <dadokkio@gmail.com>
2021-10-25 11:31:02 +0200

    funcion not in class

--- orochi/website/migrations/0038_create_superuser.py
+++ orochi/website/migrations/0038_create_superuser.py
@@ -3,25 +3,26 @@ from django.contrib.auth import get_user_model
 from allauth.account.models import EmailAddress
 
 
+def generate_superuser(app, schema_editor):
+    superusers = get_user_model().objects.filter(is_superuser=True).count()
+    if superusers == 0:
+        superuser = get_user_model().objects.create_superuser(
+            username="admin", email="admin@orochi.local", password="admin"
+        )
+        superuser.save()
+        email, _ = EmailAddress.objects.get_or_create(
+            user=superuser, email=superuser.email
+        )
+        email.verified = True
+        email.save()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
         ("website", "0037_plugin_yara_check"),
         ("ya", "0005_auto_20210618_0947"),
     ]
-
-    def generate_superuser(self, app, schema_editor):
-        superusers = get_user_model().objects.filter(is_superuser=True).count()
-        if superusers == 0:
-            superuser = get_user_model().objects.create_superuser(
-                username="admin", email="admin@orochi.local", password="admin"
-            )
-            superuser.save()
-            email, _ = EmailAddress.objects.get_or_create(
-                user=superuser, email=superuser.email
-            )
-            email.verified = True
-            email.save()
 
     operations = [
         migrations.RunPython(generate_superuser),
