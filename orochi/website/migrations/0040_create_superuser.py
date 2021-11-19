74ea56c7a513af30fa6fab6d4496ba50d1e7c1f1
dadokkio <dadokkio@gmail.com>
2021-11-19 18:42:50 +0100

    fix conflict

--- orochi/website/migrations/0040_create_superuser.py
+++ orochi/website/migrations/0040_create_superuser.py
@@ -20,11 +20,7 @@ def generate_superuser(app, schema_editor):
 class Migration(migrations.Migration):
 
     dependencies = [
-<<<<<<< HEAD:orochi/website/migrations/0040_create_superuser.py
         ("website", "0039_auto_20211119_1654"),
-=======
-        ("website", "0038_extracteddump_md5"),
->>>>>>> 96e2b53930c7095813b619f2543858e3912c886a:orochi/website/migrations/0039_create_superuser.py
         ("ya", "0005_auto_20210618_0947"),
     ]
 
