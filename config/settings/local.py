b7a455471fab386fcd720e3403b02bebf3315d06
Davide Arcuri <davide.arcuri@leonardo.com>
2024-08-28 18:06:12 +0200

    add EMAIL_TIMEOUT settings

--- config/settings/local.py
+++ config/settings/local.py
@@ -29,6 +29,7 @@ CACHES = {
 # ------------------------------------------------------------------------------
 EMAIL_HOST = env("EMAIL_HOST", default="mailpit")
 EMAIL_PORT = 1025
+EMAIL_TIMEOUT = env.int("EMAIL_TIMEOUT", default=30)
 EMAIL_USE_TLS = env("EMAIL_USE_TLS", default=True)
 EMAIL_HOST_USER = env("EMAIL_HOST_USER", default=None)
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", default=None)
